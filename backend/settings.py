7fe4deb5a78bd947b4a3207a43c4b07a459404e2
Dvooskid <dvooskid1234@gmail.com>
2024-06-15 18:09:23 +0100

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -107,7 +107,7 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-# DATABASES = {
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
