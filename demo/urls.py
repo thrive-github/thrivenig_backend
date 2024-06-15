655c367e81be5653fda9a2bc405cf8d6d8d8cca5
Dvooskid <dvooskid1234@gmail.com>
2024-06-15 17:53:33 +0100

    Update urls.py

--- demo/urls.py
+++ demo/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 
 urlpatterns = [
-    path('flight-search', views.flight, name='flight-search'),
+    path('api/flight-search', views.flight, name='flight-search'),
     path('', views.demo, name='book-flight'),
     path('origin_airport_search/', views.origin_airport_search,
          name='origin_airport_search'),
