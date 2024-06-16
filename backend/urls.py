06abff95cc8e05a13eb2be3388a8e5aa1b744147
Dvooskid <dvooskid1234@gmail.com>
2024-06-16 14:21:43 +0100

    Update urls.py

--- backend/urls.py
+++ backend/urls.py
@@ -1,8 +1,30 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf import settings
+from django.contrib.auth import views as auth_views
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+
+admin.site.site_header = "Thrivenig Superadmin"
+admin.site.site_title = "Thrivenig Superadmin Portal"
+admin.site.index_title = "Welcome to Thrivenig Superadmin Portal"
+
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("api/", include('base.urls')),
     path("", include('demo.urls')),
 ]
+
+
+# Retrieve images from /media/
+urlpatterns += staticfiles_urlpatterns()
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
