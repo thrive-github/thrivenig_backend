731f00bb578a9360f57f123c3e2e132b33d832e6
Dvooskid <dvooskid1234@gmail.com>
2024-06-16 07:41:59 +0100

    Add files via upload

--- demo/urls.py
+++ demo/urls.py
@@ -4,8 +4,9 @@ from . import views
 
 
 urlpatterns = [
+    path('', views.demo, name='home'),
     path('api/flight-search', views.flight, name='flight-search'),
-    path('', views.demo, name='book-flight'),
+
     path('origin_airport_search/', views.origin_airport_search,
          name='origin_airport_search'),
     path('destination_airport_search/', views.destination_airport_search,
